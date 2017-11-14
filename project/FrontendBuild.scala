aeccc7cc673f09bd6d2d4d07b54d79a6e54d0340
James Mountain <james.mountain@digital.hmrc.gov.uk>
2017-11-14 15:04:52 +0000

    SCRS-9362-fix Updated library dependencies

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -24,7 +24,7 @@ private object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "frontend-bootstrap" % "8.10.0",
+    "uk.gov.hmrc" %% "frontend-bootstrap" % "8.11.0",
     "uk.gov.hmrc" %% "play-partials" % "6.1.0",
     "uk.gov.hmrc" %% "url-builder" % "2.1.0",
     "uk.gov.hmrc" %% "http-caching-client" % "7.0.0",
