08c4407a153c52f954e00c56fe8df22d925b1538
Daveo-G <Daveo-G@users.noreply.github.com>
2020-05-01 09:47:29 +0100

    SAR-5499 Fix for duplicate addresses for PPOB

--- test/views/PPOBSpec.scala
+++ test/views/PPOBSpec.scala
@@ -56,21 +56,73 @@ class PPOBSpec extends SCRSSpec with PPOBFixture with NavModelRepoMock with With
   }
 
   "PPOBController.show" should {
-    "make sure that PPOB page has the correct elements when an RO address is provided to the view" in new SetupPage {
+    "make sure that the PPOB page has the correct elements when only an RO address is provided to the view" in new SetupPage {
       CTRegistrationConnectorMocks.retrieveCTRegistration()
       mockKeystoreFetchAndGet("registrationID", Some("12345"))
 
       when(mockPPOBService.fetchAddressesAndChoice(Matchers.any())(Matchers.any()))
-        .thenReturn(Future.successful(Some(CHROAddress("38", "line 1", None, "Telford", "UK", None, None, None)), Some(NewAddress("line 1", "line 2", None, None, None, None, None)), PPOBChoice("")))
+        .thenReturn(Future.successful(Some(CHROAddress("38", "line 1", Some("line 2"), "Telford", "UK", None, Some("ZZ1 1ZZ"), None)), None, PPOBChoice("")))
+
 
       showWithAuthorisedUser(controller.show) {
         result =>
           val document = Jsoup.parse(contentAsString(result))
+
           document.title() shouldBe "What is the company's 'principal place of business'?"
           document.getElementById("main-heading").text shouldBe "What is the company's 'principal place of business'?"
           document.getElementById("next").attr("value") shouldBe "Save and continue"
+
+          document.getElementById("addressChoice").getElementsByAttributeValue("for","addressChoice-ro").text() shouldBe "38 line 1, line 2, Telford ZZ1 1ZZ, UK"
+          document.getElementById("addressChoice").getElementsByAttributeValue("for","addressChoice-ppob") shouldBe empty
+          document.getElementById("addressChoice").getElementsByAttributeValue("for","addressChoice-other").text() shouldBe "A different address"
       }
     }
+    "make sure that the PPOB page has the correct elements when only an RO and different PPOB address is provided to the view" in new SetupPage {
+      CTRegistrationConnectorMocks.retrieveCTRegistration()
+      mockKeystoreFetchAndGet("registrationID", Some("12345"))
+
+      when(mockPPOBService.fetchAddressesAndChoice(Matchers.any())(Matchers.any()))
+        .thenReturn(Future.successful(Some(CHROAddress("38", "line 1", Some("line 2"), "Telford", "UK", None, Some("ZZ1 1ZZ"), None)),
+                                           Some(NewAddress("line 1", "line 2", None, None, Some("ZZ2 2ZZ"), None, None)),
+                                           PPOBChoice("PPOB")))
+
+      showWithAuthorisedUser(controller.show) {
+        result =>
+          val document = Jsoup.parse(contentAsString(result))
+
+          document.title() shouldBe "What is the company's 'principal place of business'?"
+          document.getElementById("main-heading").text shouldBe "What is the company's 'principal place of business'?"
+          document.getElementById("next").attr("value") shouldBe "Save and continue"
+
+          document.getElementById("addressChoice").getElementsByAttributeValue("for","addressChoice-ro").text() shouldBe "38 line 1, line 2, Telford ZZ1 1ZZ, UK"
+          document.getElementById("addressChoice").getElementsByAttributeValue("for","addressChoice-ppob").text() shouldBe "line 1, line 2, ZZ2 2ZZ"
+          document.getElementById("addressChoice").getElementsByAttributeValue("for","addressChoice-other").text() shouldBe "A different address"
+      }
+    }
+
+    "make sure that the PPOB page has the correct elements when the RO address was selected before" in new SetupPage {
+      CTRegistrationConnectorMocks.retrieveCTRegistration()
+      mockKeystoreFetchAndGet("registrationID", Some("12345"))
+
+      when(mockPPOBService.fetchAddressesAndChoice(Matchers.any())(Matchers.any()))
+        .thenReturn(Future.successful(Some(CHROAddress("14", "St Test Walk", Some("Testley"), "Testford", "UK", None, Some("TE1 1ST"), Some("Testshire"))),
+          Some(NewAddress("14 St Test Walk", "Testley", Some("Testford"), Some("Testshire"), Some("TE1 1ST"), Some("UK"), Some("txid"))),
+          PPOBChoice("RO")))
+
+      showWithAuthorisedUser(controller.show) {
+        result =>
+          val document = Jsoup.parse(contentAsString(result))
+
+          document.title() shouldBe "What is the company's 'principal place of business'?"
+          document.getElementById("main-heading").text shouldBe "What is the company's 'principal place of business'?"
+          document.getElementById("next").attr("value") shouldBe "Save and continue"
+
+          document.getElementById("addressChoice").getElementsByAttributeValue("for","addressChoice-ro").text() shouldBe "14 St Test Walk, Testley, Testford, Testshire, TE1 1ST, UK"
+          document.getElementById("addressChoice").getElementsByAttributeValue("for","addressChoice-ppob") shouldBe empty
+          document.getElementById("addressChoice").getElementsByAttributeValue("for","addressChoice-other").text() shouldBe "A different address"
+      }
+    }
+
     "make sure that PPOB page has the correct elements when an RO address is not provided to the view" in new SetupPage {
       CTRegistrationConnectorMocks.retrieveCTRegistration()
       mockKeystoreFetchAndGet("registrationID", Some("12345"))
