125bf0e65ef73c2d05a71e67a02f75d40ba4d5f2
Milly Harrison <64021464+HarrNM@users.noreply.github.com>
2020-11-27 14:55:39 +0000

    SAR-6742: Update continue_url in Bas gateway url

--- test/controllers/RegistrationEmailConfirmationControllerSpec.scala
+++ test/controllers/RegistrationEmailConfirmationControllerSpec.scala
@@ -123,7 +123,7 @@ class RegistrationEmailConfirmationControllerSpec extends SCRSSpec with GuiceOne
       showWithUnauthorisedUser(TestController.show()) {
         result =>
           status(result) shouldBe SEE_OTHER
-          redirectLocation(result) shouldBe Some("http://localhost:9553/bas-gateway/sign-in?continue=http%3A%2F%2Flocalhost%3A9970%2Fregister-your-company%2Fpost-sign-in&origin=company-registration-frontend")
+          redirectLocation(result) shouldBe Some("http://localhost:9553/bas-gateway/sign-in?continue_url=http%3A%2F%2Flocalhost%3A9970%2Fregister-your-company%2Fpost-sign-in&origin=company-registration-frontend")
       }
     }
   }
