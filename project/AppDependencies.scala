e98ab0a241d47a9bf471dac41bcacb777422d2d6
Michele Satta <3033136+msatta@users.noreply.github.com>
2025-08-07 14:42:46 +0100

    [DL-17026] Address jackson-core vulnerability

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,27 +4,26 @@ import sbt.*
 private object AppDependencies {
 
   val playVersion      = "-play-30"
-  val bootstrapVersion = "9.12.0"
-  val hmrcMongoVersion = "2.6.0"
-  val scalaTestVersion = "3.2.18"
+  val bootstrapVersion = "10.0.0"
+  val hmrcMongoVersion = "2.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"       %% s"bootstrap-frontend$playVersion"            % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo$playVersion"                    % hmrcMongoVersion,
     "uk.gov.hmrc"       %% s"play-partials$playVersion"                 % "10.0.0",
-    "uk.gov.hmrc"       %% s"http-caching-client$playVersion"           % "12.1.0",
+    "uk.gov.hmrc"       %% s"http-caching-client$playVersion"           % "12.2.0",
     "uk.gov.hmrc"       %% s"play-conditional-form-mapping$playVersion" % "3.3.0",
     "org.bitbucket.b_c"  % "jose4j"                                     % "0.9.6",
-    "commons-validator"  % "commons-validator"                          % "1.9.0",
+    "commons-validator"  % "commons-validator"                          % "1.10.0",
     "uk.gov.hmrc"       %% "play-language"                              % "8.1.0",
-    "uk.gov.hmrc"       %% s"play-frontend-hmrc$playVersion"            % "11.13.0"
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc$playVersion"            % "12.8.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% s"bootstrap-test$playVersion"  % bootstrapVersion % Test,
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-test$playVersion" % hmrcMongoVersion % Test,
-    "org.jsoup"          % "jsoup"                        % "1.19.1"         % Test
+    "org.jsoup"          % "jsoup"                        % "1.21.1"         % Test
   )
 
   def apply(): Seq[ModuleID] = compile ++ test
