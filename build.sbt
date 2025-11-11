7279cbe46c2102f6131f0f332de84ded19f71e3c
Michele Satta <3033136+msatta@users.noreply.github.com>
2025-11-11 09:34:13 +0000

    [DL-17829] Upgrade play-frontend-hmrc in preparation for GovUK frontend rebrand

--- build.sbt
+++ build.sbt
@@ -13,7 +13,7 @@ val appName = "company-registration-frontend"
 lazy val appDependencies: Seq[ModuleID] = AppDependencies()
 
 ThisBuild / majorVersion := 3
-ThisBuild / scalaVersion := "2.13.16"
+ThisBuild / scalaVersion := "2.13.17"
 
 lazy val scoverageSettings = {
   // Semicolon-separated list of regexs matching classes to exclude
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,26 +4,26 @@ import sbt.*
 private object AppDependencies {
 
   val playVersion      = "-play-30"
-  val bootstrapVersion = "10.1.0"
-  val hmrcMongoVersion = "2.7.0"
+  val bootstrapVersion = "10.4.0"
+  val hmrcMongoVersion = "2.10.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"       %% s"bootstrap-frontend$playVersion"            % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo$playVersion"                    % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% s"play-partials$playVersion"                 % "10.1.0",
+    "uk.gov.hmrc"       %% s"play-partials$playVersion"                 % "10.2.0",
     "uk.gov.hmrc"       %% s"http-caching-client$playVersion"           % "12.2.0",
     "uk.gov.hmrc"       %% s"play-conditional-form-mapping$playVersion" % "3.3.0",
     "org.bitbucket.b_c"  % "jose4j"                                     % "0.9.6",
     "commons-validator"  % "commons-validator"                          % "1.10.0",
     "uk.gov.hmrc"       %% "play-language"                              % "8.1.0",
-    "uk.gov.hmrc"       %% s"play-frontend-hmrc$playVersion"            % "11.13.0"
+    "uk.gov.hmrc"       %% s"play-frontend-hmrc$playVersion"            % "12.20.0"
   )
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% s"bootstrap-test$playVersion"  % bootstrapVersion % Test,
     "uk.gov.hmrc.mongo" %% s"hmrc-mongo-test$playVersion" % hmrcMongoVersion % Test,
-    "org.jsoup"          % "jsoup"                        % "1.21.1"         % Test
+    "org.jsoup"          % "jsoup"                        % "1.21.2"         % Test
   )
 
   def apply(): Seq[ModuleID] = compile ++ test
